5452c92a27f3fe74075cf053289022acac2e8420
Mithila Malinda <mithilam@99x.lk>
2020-04-29 16:58:15 +0530

    New page added, New animations added

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,30 @@ $('.main-nav').on('click', '.main-nav__search-button__icon', function (e) {
 
     e.preventDefault();
 });
+
+//tab custom js
+
+$.fn.responsiveTabs = function () {
+
+    return this.each(function () {
+        var el = $(this),
+            tabs = el.find('.dev-tabs__title'),
+            content = el.find('.dev-tabs__content'),
+            placeholder = $('<div class="dev-tabs__placeholder max-w-screen-xl mx-auto pt-20 pb-20 lg:pb-32 px-2"></div>').insertAfter(el);
+
+        tabs.on('click', function () {
+            var tab = $(this);
+
+            tabs.not(tab).removeClass('active');
+            tab.addClass('active');
+
+            placeholder.html(tab.next().html());
+        });
+
+        tabs.filter(':first').trigger('click');
+    });
+}
+
+$('.dev-tabs').responsiveTabs();
+
+AOS.init();
